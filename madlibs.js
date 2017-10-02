8fd0aacc91b8813812dc54abaf2ff40732836213
nvarney17 <nvarney@pathfinderstudents.org>
2017-10-02 14:00:42 -0400

    Add files via upload

--- madlibs.js
+++ madlibs.js
@@ -9,13 +9,23 @@ alert ("Enter after each word. End with Shift + 6 (^)");
 populate(index, sentence, "word");
 alert (sentence);
 index = 0;
-//specify positions
+//fill or specify positions
 populate(index,replacements, "position");
 alert (replacements);
+index = 0;
 replace(index, sentence, replacements, "part of speech");
-
 alert (sentence);
-
+// switch people
+//train filler
+alert ("Enter words that match the part of speech shown.");
+index = 0;
+replace(index, sentence, replacements, "new word");
+alert (sentence);
+//show the sentence
+alert ("Go get the author to see the sentence");
+sentenceString = sentence.join(' ');
+alert(sentenceString.toString());
+//end
 
 function populate(sindex,sarray,label) {
 	do {
@@ -30,7 +40,12 @@ function populate(sindex,sarray,label) {
 
 function replace(sindex, sarray, rarray, label) {
 	for (sindex = 0; sindex < rarray.length; sindex++) {
-		sarray[rarray[sindex]] = prompt("Next "+label+"?");
+			if (label == "new word") {
+				sarray[rarray[sindex]] = prompt("Provide a "+sarray[rarray[sindex]]+" here: ");
+			}
+			else {
+				sarray[rarray[sindex]] = prompt("Next "+label+"?");
+		}
 	}
 	return sarray;
 }
